4befd7182bb3b6ad5320872587e0d95d4acf622e
rufet <Elchinoglu.Rufat@gmail.com>
2023-06-22 15:23:45 +0400

    first message

---
+++ Pages/Account/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication12.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
